cc7f7dbabe06c69ac968e9454241729a5984a65b
Hidran Arias <hidran@gmail.com>
2021-08-07 16:09:13 +0200

    implements fetch call to movies API and sets the state with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 
+
 import './App.css';
-const APIKEY = 'xxxxxx';
+import {useState, useEffect} from 'react';
+const APIKEY = '4cb9def9';
+
+
+
+const APIURL = 'https://www.omdbapi.com';
+
+const fetchMovies = async (search = 'The godfather') => {
+  const response = await fetch(APIURL + '?apikey=' + APIKEY + '&s=' + search).then(res => res.json());
+  const { Search: movies, totalResults: totalCount } = response;
+  console.log(response);
+  return { movies, totalCount };
+}
+
+
 function App() {
+     
+  const [movies, setMovies] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    const callApi = async () => {
+      const data = await fetchMovies();
+      setMovies(data.movies);
+      setTotalCount(data.totalCount);
+    }
+    callApi();
+    return () => {
+     
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-       <h1> MY FAVORITE MOVIES</h1>
+        <h1> MY FAVORITE MOVIES</h1>
+        <ul>
+          {movies.map(movie => <li key={movie.imbID}>{ movie.Title}</li>)}
+        </ul>
       </header>
     </div>
   );
